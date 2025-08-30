82ef240b18ea5bc1000ee9805be8474297581de2
eduardo <eduardomendes418@gmail.com>
2025-08-30 20:15:00 -0300

    EPM-06] fix: layout and login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,9 +22,12 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(routes)/login/index" />
+        <Stack.Screen
+          name="(routes)/login/index"
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
