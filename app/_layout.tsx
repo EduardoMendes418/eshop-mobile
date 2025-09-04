2242381290129f239b486a87776760cd47e4ae37
eduardo <eduardomendes418@gmail.com>
2025-09-03 21:19:32 -0300

    EPM-10] fix: install tailwindcss

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,21 @@
 import {
+  useFonts
+} from "@expo-google-fonts/poppins";
+import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { useFonts } from "expo-font";
+
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
+import "./globals.css";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme(); 
+  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
